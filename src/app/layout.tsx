d29bfc85cd70ad1b87d02ada367cd815c6eb828d
tttm <80350771+33tm@users.noreply.github.com>
2024-02-18 19:07:04 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
     title: "hw",
-    description: "Hardy-Weinberg Calculator"
+    description: "Hardy-Weinberg Evaluator // 33tm"
 }
 
 export default ({ children }: Readonly<{ children: React.ReactNode }>) => {
